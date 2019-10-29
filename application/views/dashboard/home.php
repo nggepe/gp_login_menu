56422f2886993a67f51bd64c8bfe0a3463bc18ca
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2019-10-29 21:52:44 +0700

    project name

--- application/views/dashboard/home.php
+++ application/views/dashboard/home.php
@@ -7,7 +7,7 @@ $this->load->view('admin_design/header', $modul);
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-        <h1>Tani Agung Administration</h1>
+        <h1>Your Administration</h1>
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
             <div class="breadcrumb-item"><a href="#">Layout</a></div>
